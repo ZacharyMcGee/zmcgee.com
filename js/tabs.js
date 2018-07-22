b6a093d2be6a95b8c263a66b9145f891a2b54842
Zachary McGee <zachary.mcgee@siu.edu>
2018-07-22 01:56:19 -0500

    Coursework Tab

--- js/tabs.js
+++ js/tabs.js
@@ -1,3 +1,5 @@
+var currentPage = "1";
+
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -10,4 +12,26 @@ function openTab(evt, tabName) {
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+function decrementCoursePage(pageNum){
+
+}
+
+function coursePage(pageNum, coursePage) {
+    var i, tabcontent, tablinks, dots;
+    document.getElementById(currentPage).classList.remove('active');
+    document.getElementById("li" + currentPage).classList.remove("active");
+    currentPage = pageNum;
+    tabcontent = document.getElementsByClassName("coursePages");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+
+
+
+    document.getElementById(coursePage).style.display = "block";
+    document.getElementById("li" + pageNum).classList.add("active");
+    document.getElementById(pageNum).classList.add('active');
 }
