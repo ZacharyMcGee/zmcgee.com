444e10991824721622738ec76e5841b0c93f4791
Zachary McGee <zachary.mcgee@siu.edu>
2018-07-25 03:30:04 -0500

    Start Image Post Template

--- post.php
+++ post.php
@@ -3,6 +3,6 @@
   $sql = "SELECT id, category, title, post, featuredimg FROM Posts WHERE id='$post'";
   $result = $conn->query($sql);
   $row = $result->fetch_assoc();
-  echo "<div class=\"image-post\"><img src=\"" . $row["featuredimg"] . "\"></div>";
+  echo "<div class=\"post-container\"><div class=\"left-post\"><img src=\"" . $row["featuredimg"] . "\"></div><div class=\"right-post\"><div class=\"right-post-header\">" . $row["title"] . "</div><div class=\"right-post-body\"></div></div></div>";
   ?>
 </div>
