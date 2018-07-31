7639695ac7e3acd32d4114c9c1146d7a5870a0af
Zachary McGee <zachary.mcgee@siu.edu>
2018-07-31 03:28:32 -0500

    Update Article Style

--- post.php
+++ post.php
@@ -1,14 +1,14 @@
 <div id="container" class="container">
   <?php
-  $sql = "SELECT id, category, title, post, posttype, featuredimg FROM Posts WHERE id='$post'";
+  $sql = "SELECT id, category, title, post, posttype, source, featuredimg FROM Posts WHERE id='$post'";
   $result = $conn->query($sql);
   $row = $result->fetch_assoc();
 
   if($row["posttype"] == "image"){
-    echo "<div class=\"post-container\"><div class=\"left-post\"><img src=\"" . $row["featuredimg"] . "\"></div><div class=\"right-post\"><div class=\"right-post-header\">" . $row["title"] . "</div><div class=\"right-post-body\">" . $row["post"] . "</div></div></div>";
+    echo "<div class=\"image-post-container\"><div class=\"left-post\"><img src=\"" . $row["featuredimg"] . "\"></div><div class=\"right-post\"><div class=\"right-post-header\">" . $row["title"] . "</div><div class=\"right-post-body\">" . $row["post"] . "</div></div></div>";
   }
   else if($row["posttype"] == "article"){
-    echo "<div class=\"post-container\"><div class=\"article-post\"><div class=\"article-post-header\">article" . $row["title"] . "</div><div class=\"article-post-body\">" . $row["post"] . "</div></div></div>";
+    echo "<div class=\"article-post-container\"><div class=\"article-post\"><div class=\"article-post-header\">" . $row["title"] . " <span class=\"article-post-header-sourcelink\"><a href=\"" . $row["source"] . "\">View Source</a></span></div><div class=\"article-post-body\">" . html_entity_decode($row["post"]) . "</div></div></div>";
   }
   ?>
 </div>
